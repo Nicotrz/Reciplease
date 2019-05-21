5f917cf099f213894582dfac5197466210ff58c2
Nicotrz <nicolas.trz@gmail.com>
2019-05-21 21:25:16 +0200

    Building user interface for the search view

---
+++ Reciplease/Controller/SearchPageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchPageViewController.swift
+//  Reciplease
+//
+//  Created by Nicolas Sommereijns on 21/05/2019.
+//  Copyright © 2019 Nicolas Sommereijns. All rights reserved.
+//
+
+import UIKit
+
+class SearchPageViewController: UIViewController {
+
+}
