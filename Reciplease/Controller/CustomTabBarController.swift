875ff86ff3e0b78a45b525024988e1a0f4a40418
Nicotrz <nicolas.trz@gmail.com>
2019-05-30 16:01:33 +0200

    fixed custom tab bar

--- Reciplease/Controller/CustomTabBarController.swift
+++ Reciplease/Controller/CustomTabBarController.swift
@@ -14,28 +14,14 @@ class CustomTabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tabBarHeight = self.tabBar.frame.size.height
-        let window = UIApplication.shared.windows[0]
-        let safeFrame = window.safeAreaLayoutGuide.layoutFrame
-        let bottomSafeAreaHeight = window.frame.maxY - safeFrame.maxY
         customTabBarView.frame.size.width = self.view.frame.width
-        customTabBarView.frame.size.height = tabBarHeight
+        customTabBarView.frame.size.height = 70
         self.view.addSubview(customTabBarView)
-        customTabBarView.frame.origin.y = self.view.frame.height - bottomSafeAreaHeight - customTabBarView.frame.height
+        customTabBarView.frame.origin.y = self.view.frame.height - customTabBarView.frame.height
     }
     
     @IBAction func changeTab(_ sender: UIButton) {
         self.selectedIndex = sender.tag
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
